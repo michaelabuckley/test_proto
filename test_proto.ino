5dedfdf39707d4611b4a6578a313a6fb6952304e
Michael Buckley <michaelabuckley@gmail.com>
2023-02-20 12:48:58 -0500

    Start extracting Sample struct

--- test_proto.ino
+++ test_proto.ino
@@ -15,38 +15,48 @@ int count = 0;
 int p_test = 5;
 float r_ref = 3570.0F;
 
-uint32_t adc_AA1;
-uint32_t adc_AB;
-uint32_t adc_AC;
-uint32_t adc_BB1;
-uint32_t adc_BC;
-uint32_t adc_CC1;
 
-uint32_t r_AA1;
-uint32_t r_AB;
-uint32_t r_AC;
-uint32_t r_BB1;
-uint32_t r_BC;
-uint32_t r_CC1;
+int32_t r_AA1;
+int32_t r_AB;
+int32_t r_AC;
+int32_t r_BB1;
+int32_t r_BC;
+int32_t r_CC1;
 
 // adc read with pin low
-uint32_t calib_floor;
+int32_t calib_floor;
 // adc read with pin 10 ohm to low
-uint32_t calib_10_delta;
+int32_t calib_10_delta;
 #define CALIB_10 10
 
+// 6 samples we collect.  ADC samples are < 16 bits.  int32 is large enough to do sums
+typedef union {
+  struct {
+    int32_t sAA1;
+    int32_t sAB;
+    int32_t sAC;
+    int32_t sBB1;
+    int32_t sBC;
+    int32_t sCC1;
+  };
+  int32_t sample[6];
+} Sample;
 
-uint32_t covert_to_ohms(uint32_t sample) {
-  uint32_t result = ((sample - calib_floor) * CALIB_10 * 10) / (calib_10_delta);
-  // if (result > 9999) {
-  //   result = 9999;
-  // }
+Sample adc = {0};
+
+int32_t covert_to_ohms(uint32_t sample) {
+
+  int32_t result = ((((int32_t)sample) - calib_floor) * CALIB_10 * 10) / (calib_10_delta);
+  if (result > 9999) {
+    result = 9999;
+  }
   return result;
 }
 
 void calibrateLevels() {
-  digitalWrite(PIN_TADrv, HIGH);
   pinMode(PIN_TADrv, OUTPUT);
+  digitalWrite(PIN_TADrv, HIGH);
+  delay(1);
 
   calib_floor = samplePinWithPinLow(PIN_TA_10, PIN_TA);
   calib_10_delta = samplePinWithPinLow(PIN_TA, PIN_TA_10) - calib_floor;
@@ -96,16 +106,22 @@ void setup() {
 void loop() {
   //Serial.println("loop()");
 
-  samplePins();
+  samplePins(&adc);
 
-  Serial.printf("raw adc_BB1: %4d\n", adc_BB1);
+  //Serial.printf("raw adc_BB1: %4d\n", adc_BB1);
 
-  r_AA1 = covert_to_ohms(adc_AA1);
-  r_AB = covert_to_ohms(adc_AB);
-  r_AC = covert_to_ohms(adc_AC);
-  r_BB1 = covert_to_ohms(adc_BB1);
-  r_BC = covert_to_ohms(adc_BC);
-  r_CC1 = covert_to_ohms(adc_CC1);
+  r_AA1 = covert_to_ohms(adc.sAA1);
+  r_AB = covert_to_ohms(adc.sAB);
+  r_AC = covert_to_ohms(adc.sAC);
+  r_BB1 = covert_to_ohms(adc.sBB1);
+  r_BC = covert_to_ohms(adc.sBC);
+  r_CC1 = covert_to_ohms(adc.sCC1);
+
+  if (r_BB1 > 1000) {
+  Serial.printf("raw adc.sBB1: %4d\n", adc.sBB1);
+  Serial.printf("raw r_BB1: %4d\n", r_BB1);
+  Serial.printf("calib: %4d %4d\n", calib_floor, calib_10_delta);
+  }
 
  // double height.
   display.setTextSize(1,2);
@@ -116,37 +132,37 @@ void loop() {
 
   display.printf("A %4d  B %4d C %4d\n", r_AA1, r_BB1, r_CC1);
   display.printf("AB%4d AC %4d BC%4d\n", r_AB, r_AC, r_BC);
-  // display.printf("A %4d  B %4d C %4d\n", adc_AA1, adc_BB1, adc_CC1);
-  // display.printf("AB%4d AC %4d BC%4d\n", adc_AB, adc_AC, adc_BC);
+  // display.printf("A %4d  B %4d C %4d\n", adc.AA1, adc.BB1, adc.CC1);
+  // display.printf("AB%4d AC %4d BC%4d\n", adc.AB, adc.AC, adc.BC);
 
   display.display();
 
 }
 
 
-void samplePins() {
+void samplePins(Sample *sample) {
 
   digitalWrite(PIN_TADrv, HIGH);
   pinMode(PIN_TADrv, OUTPUT);
 
-  adc_AA1 = samplePinWithPinLow(PIN_TA, PIN_TA1);
-  adc_AB = samplePinWithPinLow(PIN_TA, PIN_TB);
-  adc_AC = samplePinWithPinLow(PIN_TA, PIN_TC);
+  sample->sAA1 = samplePinWithPinLow(PIN_TA, PIN_TA1);
+  sample->sAB = samplePinWithPinLow(PIN_TA, PIN_TB);
+  sample->sAC = samplePinWithPinLow(PIN_TA, PIN_TC);
 
   pinMode(PIN_TADrv, INPUT);
 
   digitalWrite(PIN_TBDrv, HIGH);
   pinMode(PIN_TBDrv, OUTPUT);
 
-  adc_BB1 = samplePinWithPinLow(PIN_TB, PIN_TB1);
-  adc_BC = samplePinWithPinLow(PIN_TB, PIN_TC);
+  sample->sBB1 = samplePinWithPinLow(PIN_TB, PIN_TB1);
+  sample->sBC = samplePinWithPinLow(PIN_TB, PIN_TC);
 
   pinMode(PIN_TBDrv, INPUT);
 
   digitalWrite(PIN_TCDrv, HIGH);
   pinMode(PIN_TCDrv, OUTPUT);
 
-  adc_CC1 = samplePinWithPinLow(PIN_TC, PIN_TC1);
+  sample->sCC1 = samplePinWithPinLow(PIN_TC, PIN_TC1);
 
   pinMode(PIN_TCDrv, INPUT);
 
