7fe890c7020707844393ea859e8220808929ce09
Michael Buckley <michaelabuckley@gmail.com>
2023-03-19 19:06:16 -0400

    Add libraries to Sloeber

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,12 +2,25 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2023-03-19 18:57:58
+//This file has been generated on 2023-03-19 19:03:11
 
 #include "Arduino.h"
-#include "Arduino.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "board_wiring.h"
+#include "adc.h"
+#include "oled.h"
 
+void pinSetup() ;
 void setup() ;
+void draw_all_samples(Sample *r) ;
+void display_msg(const char *s) ;
+void draw_msg(const char *s) ;
+void draw_calibration() ;
+void change_mode() ;
+void display_current_mode() ;
 void loop() ;
 
 #include "test_proto.ino"
